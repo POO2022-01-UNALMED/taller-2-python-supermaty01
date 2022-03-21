68f6320373ae1bf896b36f184f1e0dde259033da
Activeone/mateo.alvarez <mateo.alvarez@activeone.co>
2022-03-20 23:31:49 -0500

    Añade archivo

---
+++ main.py
@@ -0,0 +1,47 @@
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+        
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+        
+    def asignarTipo(self, tipo):
+        if tipo == 'electrico' or tipo == 'gasolina':
+            self.tipo == tipo
+
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+        
+    def cambiarColor(self, color):
+        if color in ['rojo', 'verde', 'amarillo', 'negro', 'blanco']:
+            self.color = color
+
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        
+    def cantidadAsientos(self):
+        numAsientos = 0
+        for asiento in self.asientos:
+            if isinstance(asiento, Asiento):
+                numAsientos+=1
+        return numAsientos
+    
+    def verificarIntegridad(self):
+        for asiento in self.asientos:
+            if isinstance(asiento, Asiento) and (asiento.registro != self.registro):
+                return 'Las piezas no son originales'
+        if self.motor.registro != self.registro:
+            return 'Las piezas no son originales'     
+        return 'Auto original'
